3e6ee2da1ef52ab20a8045ad360d619fe7082422
rodri <daniadri963@gmail.com>
2024-07-15 22:18:15 +0200

    clase Direccion

---
+++ pythonProject2/clases.py
@@ -0,0 +1,18 @@
+class Direccion:
+    def __init__(self, calle, piso, ciudad, codigo_postal):
+        self.calle = calle
+        self.piso = piso
+        self.ciudad = ciudad
+        self.codigo_postal = codigo_postal
+
+
+class Persona:
+
+class Telefono:
+    
+class Contacto:
+
+class Agenda:
+
+
+
